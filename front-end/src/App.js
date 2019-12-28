4710ce2ccc5b1c19fe09db266a671553d12117d4
weijia0208 <1002475276@qq.com>
2019-12-28 11:05:17 +0800

    框架结构

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>小区物业管理系统</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
